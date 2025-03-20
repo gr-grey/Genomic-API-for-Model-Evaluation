da103cc73d008e0b98c15e349845221eacbdd910
Satyam Priyadarshi <satyam.priyadarshi@ubc.ca>
2025-03-19 17:14:02 -0700

    Updated max_length from 2k to 400k

--- src/training_examples/Apptainer/Test_Evaluator_Predictor/predictor_container_sample/deBoerTest_model.py
+++ src/training_examples/Apptainer/Test_Evaluator_Predictor/predictor_container_sample/deBoerTest_model.py
@@ -7,13 +7,13 @@ import base64
 
 ## model specific checks that cause a "prediction_request_failed" error
 def check_seqs_specifications(sequences, json_return_error_model):
-    max_length = 2000
+    max_length = 400000
     for sequence in sequences:
         value = sequences[sequence]
         key = sequence
 
-        if len(value) > 2000:
-            json_return_error_model['prediction_request_failed'].append("length of a sequence in " + key + " is greater than 2000")
+        if len(value) > max_length:
+            json_return_error_model['prediction_request_failed'].append(f"length of a sequence in " + key + " is greater than " + max_length + "bases")
         if "N" in value:
             json_return_error_model['prediction_request_failed'].append("sequence in " + key + " has an invalid character present")
     return(json_return_error_model)
